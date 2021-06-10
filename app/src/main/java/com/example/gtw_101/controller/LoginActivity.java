f84e814ef28aa657b8389f21af6b5cb5dd41c2aa
dcongtoai <57785250+dcongtoai@users.noreply.github.com>
2021-06-10 11:28:50 +0700

    sua lai

--- app/src/main/java/com/example/gtw_101/controller/LoginActivity.java
+++ app/src/main/java/com/example/gtw_101/controller/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        getSupportActionBar().getCustomView();
+        getSupportActionBar().hide();
     }
 
     public void registerIntent(View view) {
