fa73841b65ed1bd07bfbee445be6c5bdfd144654
dcongtoai <57785250+dcongtoai@users.noreply.github.com>
2021-07-07 12:39:53 +0700

    tao them achievement

---
+++ app/src/main/java/com/example/gtw_101/controller/AchievementActivity.java
@@ -0,0 +1,15 @@
+package com.example.gtw_101.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gtw_101.R;
+
+import android.os.Bundle;
+
+public class AchievementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement);
+    }
+}
