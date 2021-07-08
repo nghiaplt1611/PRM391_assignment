2a4fe29fc8a298f5910255b9090319e9d49583fa
dcongtoai <57785250+dcongtoai@users.noreply.github.com>
2021-07-08 16:37:40 +0700

    Sửa lại achievement + thêm trang leaderboard

---
+++ app/src/main/java/com/example/gtw_101/controller/LeaderActivity.java
@@ -0,0 +1,15 @@
+package com.example.gtw_101.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.gtw_101.R;
+
+public class LeaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader);
+    }
+}
---
+++ app/src/main/java/com/example/gtw_101/fragment/HowToPlayFirstFragment.java
@@ -0,0 +1,67 @@
+package com.example.gtw_101.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gtw_101.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HowToPlayFirstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HowToPlayFirstFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HowToPlayFirstFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HowToPlayFirstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HowToPlayFirstFragment newInstance(String param1, String param2) {
+        HowToPlayFirstFragment fragment = new HowToPlayFirstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_how_to_play_first, container, false);
+        return view;
+    }
+}
